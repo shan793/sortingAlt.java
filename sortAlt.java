e94d3640d7a56998cd44e4cb8ce040964bff4c41
sg09393 <sgoonatilaka09@gmail.com>
2021-10-03 18:04:32 -0400

    First commit

---
+++ sortAlt.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String sortString(String s) {
+        int[] alphabet = new int[26];
+        for(char ch: s.toCharArray()){
+            alphabet[ch - 'a'] +=1;
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < s.length()) {
+            for (int i = 0; i < 26; i++){
+                if(alphabet[i] > 0) {
+                    sb.append((char) (i + 'a'));
+                    alphabet[i] -=1;
+                }
+            }
+            for(int i = 25; i >= 0 ; i --){
+                if (alphabet[i] >0) {
+                sb.append((char) (i + 'a'));
+                    alphabet[i] -=1;
+                }
+            }
+
+
+            }
+        return sb.toString();
+    }
+}
